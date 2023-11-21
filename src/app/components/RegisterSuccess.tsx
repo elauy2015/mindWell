b2273f09dfbb898205fdef1aa7822f096627d52c
Cristian Bula Casa <c.bula@gotuuri.com>
2023-11-20 19:06:47 -0500

    fix design

--- src/app/components/RegisterSuccess.tsx
+++ src/app/components/RegisterSuccess.tsx
@@ -13,10 +13,10 @@ const RegisterSucccess = ({isOpen, onOpenChange}:RegisterSucccess) => {
       {(onClose) => (
         <>
           <ModalBody>
-           <h3 className="text-xl my-12 text-center">Muchas gracias por registrarse</h3>
+           <h3 className="text-3xl my-12 text-center">Muchas gracias por registrarse</h3>
            <Link href="/chat"><Button
             size="lg"
-            className=" !bg-principal w-full text-white text-2xl mt-32 h-14 rounded-2xl font-bold "
+            className=" !bg-principal w-full text-white text-2xl mb-8 h-14 rounded-2xl font-bold "
           >
             Start Chat
           </Button></Link>
